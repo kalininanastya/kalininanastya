5c1431be9511c0edb4fcf7ca194d79f2dd1c61d8
kalininanastya <90443633+kalininanastya@users.noreply.github.com>
2021-11-05 14:53:11 +0300

    Update script.js

--- 5/script.js
+++ 5/script.js
@@ -1,12 +1,12 @@
 
-         function clickA() {
-         let f1 = document.getElementsByName("field1");
-         let f2 = document.getElementsByName("field2");
-         let r = document.getElementById("result");
-         r.innerHTML = f1[0].value * f2[0].value;
-         if ((f2[0].value == '0') || (f1[0].value == '0')) {
-             r.innerHTML = "error";
-             alert(" Недопустимые значения ");
-         }
-         return false;
-     }
+       function click1() {
+   let f1 = document.getElementsByName("field1");
+   let f2 = document.getElementsByName("field2");
+   let r = document.getElementById("result");
+   r.innerHTML = f1[0].value * f2[0].value;
+   if((f2[0].value=='0')||(f1[0].value=='0')){
+       r.innerHTML="ERROR";
+       alert("НЕДОПУСТИМЫЕ ЗНАЧЕНИЯ");
+   }
+   return false;
+}
